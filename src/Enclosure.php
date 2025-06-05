e18cb9ddc2def99ddc2992ff3e919014d5ecf44c
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-06-05 16:37:12 +0200

    fix big big bug (space overflow)

--- src/Enclosure.php
+++ src/Enclosure.php
@@ -15,7 +15,7 @@ class Enclosure
     {
         $answer = "";
         foreach ($this->animals as $animal) {
-            $answer .= $animal->getName() . " who is a  " .substr(get_class($animal),12)." and makes ".$animal->getNoise() . "\n";
+            $answer .= $animal->getName() . " who is a " .substr(get_class($animal),12)." and makes ".$animal->getNoise() . " !\n";
         }
         return $answer;
     }
